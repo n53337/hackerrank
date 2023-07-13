7889a98cd633b57187a5e332c431ba4b807dacb7
u53f <scalewithusef@gmail.com>
2023-07-13 13:41:59 +0100

    solve: Java inheritance I init: Java Abstract Class

---
+++ Java Inheritance I/src/Bird.java
@@ -0,0 +1,9 @@
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
+
+    void sign(){
+        System.out.println("I am singing");
+    }
+}
---
+++ Java Inheritance I/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sign();
+    }
+}
